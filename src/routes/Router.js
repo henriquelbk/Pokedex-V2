5e06666a191cef946880b22ce872eacf4aec8791
henriquelbk <loubackhenrique123@gmail.com>
2024-04-02 22:13:03 -0300

    Routes done

---
+++ src/routes/Router.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PokedexPage from "../Pages/PokedexPage/PokedexPage";
+import PokemonDetailPage from "../Pages/PokemonDetailPage/PokemonDetailPage";
+import PokemonsListPage from "../Pages/PokemonsListPage/PokemonsListPage";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Footer from "../Components/Footer/Footer";
+
+const Router = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PokemonsListPage />} />
+          <Route path="/pokedex" element={<PokedexPage />} />
+          <Route path='/pokemondetails/:id' element={<PokemonDetailPage/>} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  );
+};
+
+export default Router;
---
+++ src/routes/coordinator.js
@@ -0,0 +1,11 @@
+export const goToPokedex = (navigate) => {
+    navigate('/pokedex')
+}
+
+export const goToHomePage = (navigate) => {
+    navigate('/')
+}
+
+export const goToDetailsPage = (navigate) => {
+    navigate(`/pokemondetails/${id}`)
+}
