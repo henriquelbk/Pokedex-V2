39067f57807a3ec0184cf647b9da988ba472d71e
henriquelbk <loubackhenrique123@gmail.com>
2024-04-07 20:40:26 -0300

    Paves-v2*

---
+++ src/Pages/PokedexPage/PokedexPage.js
@@ -0,0 +1,5 @@
+import { useContext, useEffect } from 'react';
+import { Button, Title, Container1, Container2} from './pokedexPageStylet';
+import { PokeContext } from '../../contexts/PokeContext';
+import { PokemonCard } from './PokemonCard';
+import { Header } from './Header';
