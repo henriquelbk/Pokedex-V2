5c7ad1a140eec0c9213941ab4d497dd42bd97158
henriquelbk <loubackhenrique123@gmail.com>
2024-04-08 22:30:57 -0300

    Pages-v3

--- src/Pages/PokedexPage/PokedexPage.js
+++ src/Pages/PokedexPage/PokedexPage.js
@@ -1,5 +1,75 @@
-import { useContext, useEffect } from 'react';
-import { Button, Title, Container1, Container2} from './pokedexPageStylet';
-import { PokeContext } from '../../contexts/PokeContext';
-import { PokemonCard } from './PokemonCard';
-import { Header } from './Header';
+import { useContext, useEffect } from "react";
+import { Button, Title, Container1, Container2 } from "./pokedexPageStylet";
+import { PokeContext } from "../../contexts/PokeContext";
+import { PokemonCard } from "./PokemonCard";
+import { useNavigate } from "react-router-dom";
+import { goToHome } from "../../routes/cordinator";
+import { Header } from "./Header";
+import {
+  Modal,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import modalremover from "../../assets/modalremover.png";
+
+const PokedexPage = () => {
+  const {
+    pokedex,
+    getPokemons,
+    addToPokedex,
+    removeFromPokedex,
+    closeModal,
+    isOpen,
+    getItemsLocalStorage,
+  } = useContext(PokeContext);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getItemsLocalStorage();
+  }, []);
+
+
+return (
+    <Container1>
+      <Header />
+        <Title>Meus Pokémons</Title>
+        <Container2>
+          {pokedex.length === 0 ? (
+            <div>
+              <h2>Você não possui pokémons na sua pokedex. </h2>
+              <Button onClick={()=> goToHome(navigate)}>Capturar Pokémons!</Button>
+            </div>
+          ) : (
+            pokedex.map((item) => {
+              const types = item.data.types.map((types) => types.type.name);
+              return (
+                <PokemonCard
+                  getPokemons={getPokemons}
+                  pokemon={item}
+                  image={
+                    item.data.sprites.other["official-artwork"].front_default
+                  }
+                  name={item.data.name}
+                  id={item.data.id}
+                  key={item.data.id}
+                  type={types}
+                  addToPokedex={addToPokedex}
+                  removeFromPokedex={removeFromPokedex}
+                />
+              );
+            })
+          )}
+          <Modal isOpen={isOpen} onClose={closeModal}>
+            <ModalOverlay />
+            <ModalContent top={'35%'}>
+              <img src={modalremover} alt="" />
+              <ModalCloseButton />
+            </ModalContent>
+          </Modal>
+        </Container2>
+    </Container1>
+  );
+};
+export default PokedexPage;
---
+++ src/Pages/PokemonListPage/PokemonListPage.js
@@ -0,0 +1,52 @@
+import PokemonCard from "../../Components/PokemonCard/PokemonCard";
+import { Container, Main } from "./pokemonListStyle";
+import { PokeContext } from "../../contexts/PokeContext";
+import modalcapturar from "../../assets/modalcapturar.png";
+import Header from "../../Components/Header/Header";
+import {
+  Modal,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+} from "@chakra-ui/react";
+
+const PokemonListPage = () => {
+  const { pokemon, addToPokedex, removeFromPokedex, closeModal, isOpen } =
+    useContext(PokeContext);
+
+  return (
+    <>
+      <Header />
+
+      <Main>Todos os Pokémons</Main>
+      <Container>
+        {pokemon.map((item) => {
+          const types = item.data.types.map((types) => types.type.name);
+
+          return (
+            <PokemonCard
+              pokemon={item}
+              image={item.data.sprites.other["official-artwork"].front_default}
+              name={item.data.name}
+              id={item.data.id}
+              key={item.data.id}
+              type={types}
+              addToPokedex={addToPokedex}
+              removeFromPokedex={removeFromPokedex}
+            />
+          );
+        })}
+      </Container>
+
+      <Modal isOpen={isOpen} onClose={closeModal}>
+        <ModalOverlay />
+        <ModalContent top={"35%"}>
+          <img src={modalcapturar} alt="" />
+          <ModalCloseButton />
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default PokemonListPage;
